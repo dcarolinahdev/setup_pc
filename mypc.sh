33c476d4a69c4389d7114bd27a5fc965b5e402f1
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2024-09-27 18:23:57 -0500

    Updating after last setup

--- mypc.sh
+++ mypc.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-echo "Starting"
-sudo apt update
-sudo apt upgrade
 
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "--> Installing basics"
+echo "---> Starting"
+echo "------------------------------------------"
+echo "---> Updating PC"
+sudo apt update -y
+echo "---> Upgrading PC"
+sudo apt upgrade -y
+
+mkdir Installers
+mkdir Projects
+
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "---> Installing basics"
+echo "------------------------------------------"
+echo "---> Installing curl and tree"
 sudo apt install curl tree
+echo "---> Installing gimp, imagemagic and fonts-font-awesome"
 sudo apt install gimp imagemagick fonts-font-awesome
+echo "---> Installing sqlite3 and sqlitebrowser"
 sudo apt install sqlite3 sqlitebrowser
 
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "---> Git"
+echo "------------------------------------------"
 echo "1. Installing ..."
 sudo apt install git
 
@@ -23,24 +37,28 @@ git config --global user.name ""
 
 echo "   user.email"
 git config --global user.email ""
-# git config --global merge.ff true
-# git config --global pull.ff only
+
+echo "   pull.ff"
+git config --global pull.ff only
 
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "---> Python"
+echo "------------------------------------------"
 echo "1.  Installing version 12"
 sudo apt install python3.12 python3.12-dbg python3.12-dev python3.12-doc python3.12-venv
 
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "---> Postgresql"
+echo "------------------------------------------"
 echo "1.  Installing version 16"
 sudo apt install postgresql-16 postgresql-client-16 postgresql-contrib libpq-dev postgresql-server-dev-16
 
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "---> PgAdmin"
+echo "------------------------------------------"
 echo "1. Installing the public key for the repository (if not done previously):"
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 
@@ -50,13 +68,29 @@ sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] h
 echo "3. Installing for both desktop and web modes:"
 sudo apt install pgadmin4
 
-echo "4. Configuring the webserver, if you installed pgadmin4-web:"
+echo "4. Configuring Postgres Authentication"
+sudo systemctl is-active postgresql
+sudo systemctl is-enabled postgresql
+sudo systemctl status postgresql
+sudo pg_isready
+echo "Optional update scram-sha-256 to md5 in pg_hba.conf"
+# sudo nano --user-data-dir=~/root /etc/postgresql/16/main/pg_hba.conf
+
+echo "
+sudo su - postgres
+psql
+ALTER USER postgres PASSWORD 'myPassword'; OR
+create user your_user with superuser password 'admin';
+\q
+"
+
+echo "5. Configuring the webserver, if you installed pgadmin4-web:"
 echo "/usr/pgadmin4/bin/setup-web.sh"
 
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "---> Google Chrome"
-mkdir Installers
+echo "------------------------------------------"
 cd Installers
 echo "1. Downloading ..."
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -67,7 +101,42 @@ cd ..
 
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "---> Aliases"
+echo "---> Oh my ZSH"
+echo "------------------------------------------"
+echo "1. Installing ZSH..."
+sudo apt install zsh -y
+
+echo "2. First setup for ZSH"
+echo "To start using Zsh, open a terminal and execute:"
+echo "Select option (2)"
+zsh --version
+zsh
+
+echo "3. Making ZSH my default shell ..."
+chsh -s $(which zsh)
+echo "To verify your current shell, run:"
+echo $SHELL
+
+echo "4.1 Installing OH MY ZSH"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+echo "4.2 Customizing OH MY ZSH"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+nano ~/.zshrc
+echo "replace: ZSH_THEME='jonathan'"
+echo "replace: plugins=(git zsh-autosuggestions zsh-syntax-highlighting)"
+
+echo "reloading zsh:"
+source ~/.zshrc
+
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "---> Permanent aliases"
+echo "------------------------------------------"
+echo "...  write these lines in ~/.bashrc OR ~/.zshrc"
 echo "venv-py3     ... Creating a virtualenv"
 alias venv-py3="python3 -m venv .venv"
 
@@ -82,31 +151,10 @@ alias gitlog_tree="git log --graph --abbrev-commit --decorate --date=relative --
 blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)-
 %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
-echo "------------------------------------------"
-echo "------------------------------------------"
-echo "---> Oh my ZSH"
-echo "1. Installing ..."
-sudo apt install zsh -y
-zsh --version
-echo "To start using Zsh, open a terminal and execute: zsh"
-echo "To verify your current shell, run: echo $SHELL"
+echo "upg          ... apt update and upgrade"
+alias upg="sudo apt update && sudo apt upgrade"
 
-echo "2. Plugins"
-echo "find: nano ~/.zshrc"
-echo "replace: plugins=(git zsh-autosuggestions zsh-syntax-highlighting)"
-echo "reload zsh: source ~/.zshrc"
-
-echo "3. Making Zsh my default shell ..."
-echo "try: chsh -s $('which' zsh)"
-
-
-echo "------------------------------------------"
-echo "------------------------------------------"
-echo "---> Download projects to Documents"
-mkdir Projects
-cd Projects
-echo "clone as: git clone git@github.com:dcarolinahdev/example.git"
-cd ..
+source ~/.zshrc
 
 echo "------------------------------------------"
 echo "------------------------------------------"
@@ -114,21 +162,28 @@ echo "1. Downloading Visual Studio Code ..."
 echo "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 echo "sudo apt install ./code_1.93.1-1726079302_amd64.deb"
 
+echo "... Extensions for python:
+
+Python by Microsoft
+Python Indent by Kbrose
+Python Docstring Generator by Nils Werner
+AREPL for Python
+# gitlens
+"
+
 echo "2. Installing Okular"
 sudo apt install okular
 
 echo "3. Installing Anydesk"
-echo "Go to: https://anydesk.com/es/downloads/linux"
+echo "Go to: https://anydesk.com/es/downloads/linuxecho "To verify your current shell, run: echo $SHELL""
 echo "anydesk_6.3.3-1_amd64.deb"
 
 echo "4. Configure SSH KEY"
 echo "---> Verifying SSH KEYs"
-echo "Enter ls -al ~/.ssh to see if existing SSH keys are present."
+ls -al ~/.ssh
 
 echo "---> Generating SSH KEYs"
 echo "ssh-keygen -t ed25519 -C 'your_email@example.com'"
-echo "OR"
-echo "ssh-keygen -t rsa -b 4096 -C "your_email@example.com""
 
 echo "---> Adding the SSH KEY to the SSH-agent"
 echo "1. Start the ssh-agent in the background."
@@ -137,6 +192,8 @@ echo "eval $(ssh-agent -s)"
 echo "2. Add your SSH private key to the ssh-agent."
 echo "ssh-add ~/.ssh/id_ed25519"
 
-echo "3. Add the SSH public key to your account on GitHub"
+echo "3. Copy the SSH KEY: cat ~/.ssh/id_rsa.pub"
+
+echo "4. Add the SSH public key to your account on GitHub"
 
 exit
